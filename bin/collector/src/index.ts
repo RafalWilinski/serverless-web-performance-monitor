55e7cf80c7afd4989bede8f6313259caa6a7409a
Rafal Wilinski <raf.wilinski@gmail.com>
2019-10-31 15:09:35 +0100

    Replace request with request-promise-native

--- bin/collector/src/index.ts
+++ bin/collector/src/index.ts
@@ -1,21 +1,21 @@
-import { DynamoDB } from "aws-sdk";
-import { APIGatewayProxyHandler } from "aws-lambda";
-import * as request from "request";
+import { DynamoDB } from 'aws-sdk';
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import { get } from 'request-promise-native';
 
-import "source-map-support/register";
-import Project from "../../types/Project";
-import MetricsDatapoint from "../../types/MetricsDatapoint";
+import 'source-map-support/register';
+import Project from '../../types/Project';
+import MetricsDatapoint from '../../types/MetricsDatapoint';
 
 const dynamoDB = new DynamoDB.DocumentClient();
 const response = (body: string | any, statusCode = 200) => ({
   statusCode,
-  body: typeof body === "string" ? JSON.stringify(body, null, 2) : body
+  body: typeof body === 'string' ? JSON.stringify(body, null, 2) : body,
 });
 
 const getAllProjects = async (): Promise<Project[]> => {
   const { Items } = await dynamoDB
     .scan({
-      TableName: process.env.PROJECTS_TABLE_ARN!.split('/').slice(-1)[0]
+      TableName: process.env.PROJECTS_TABLE_ARN!.split('/').slice(-1)[0],
     })
     .promise();
 
@@ -25,7 +25,7 @@ const getAllProjects = async (): Promise<Project[]> => {
 const insertMetrics = (metricsDatapoint: MetricsDatapoint) =>
   dynamoDB.put({
     TableName: process.env.METRICS_TABLE_ARN!.split('/').slice(-1)[0],
-    Item: metricsDatapoint
+    Item: metricsDatapoint,
   });
 
 const processProject = async (project: Project) => {
@@ -34,56 +34,51 @@ const processProject = async (project: Project) => {
   const healthMetric: MetricsDatapoint = {
     id: `${project.id}`,
     date: (+new Date()).toString(),
-    unit: "Reachable",
-    metricName: "Health",
-    value: 0
+    unit: 'Reachable',
+    metricName: 'Health',
+    value: 0,
   };
 
-  await new Promise(() => {
-    request[project.method](
-      project.endpoint,
-      {
-        headers: project.headers,
-        timeout: project.timeout,
-        time: project.measureRequestDetails
-      },
-      async (error: any, response: request.Response) => {
-        console.log(`Project ${project.id}; HasError: ${!!error}, Timings: ${response.timings}`);
-        healthMetric.value = error ? 0 : 1;
+  try {
+    // const requestCall = project.method === 'get' ? get : post;
+    const data = await get(project.endpoint, {
+      headers: project.headers,
+      timeout: project.timeout,
+      time: project.measureRequestDetails,
+    });
 
-        if (project.measureRequestDetails) {
-          const metricsObjects = Object.keys(response.timings!).map(
-            metricName => ({
-              id: `${project.id}#`,
-              date: (+new Date()).toString(),
-              unit: "ms",
-              metricName,
-              value: (response.timings! as any)[metricName]
-            })
-          );
+    console.log(data);
 
-          await Promise.all(metricsObjects.map(metric => insertMetrics(metric)));
-        }
+    healthMetric.value = 1;
 
-        return insertMetrics(healthMetric);
-      }
-    );
-  });
+    if (project.measureRequestDetails) {
+      const metricsObjects = Object.keys(data.timings!).map((metricName) => ({
+        id: `${project.id}#`,
+        date: (+new Date()).toString(),
+        unit: 'ms',
+        metricName,
+        value: (data.timings! as any)[metricName],
+      }));
+
+      await Promise.all(metricsObjects.map((metric) => insertMetrics(metric)));
+    }
+  } catch (error) {
+    console.error(error);
+    healthMetric.value = 0;
+  }
 
   if (project.measureLighthouseDetails) {
     console.log(`Running Lightouse test against ${project.id}, URL: ${project.endpoint}`);
     // to be added
-
-
   }
 
-  return;
+  return insertMetrics(healthMetric);
 };
 
 export const handler: APIGatewayProxyHandler = async (_, _context) => {
   const projects = await getAllProjects();
 
-  await Promise.all(projects.map(project => processProject(project)));
+  await Promise.all(projects.map((project) => processProject(project)));
 
   return response({ projects });
 };
