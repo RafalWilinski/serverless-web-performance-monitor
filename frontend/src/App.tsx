d5e5f82c5f9e0337190e42fb1659cf52e02d3b7f
Rafal Wilinski <raf.wilinski@gmail.com>
2019-11-04 13:27:44 +0100

    Change range

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,62 +1,45 @@
-import React from "react";
-import useSWR from "swr";
-import colorConvert from "color-convert";
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend
-} from "recharts";
+import React from 'react';
+import useSWR from 'swr';
+import { takeRight } from 'lodash';
+import colorConvert from 'color-convert';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 
-const apiUrl = "https://mte4gv5azj.execute-api.us-east-1.amazonaws.com/prod";
+const apiUrl = 'https://mte4gv5azj.execute-api.us-east-1.amazonaws.com/prod';
 
-const _fetch = (query: string) =>
-  fetch(`${apiUrl}${query}`).then(res => res.json());
+const _fetch = (query: string) => fetch(`${apiUrl}${query}`).then((res) => res.json());
 
 const App: React.FC = () => {
-  const { data } = useSWR("/projects", _fetch);
-  const { data: metricsData, error } = useSWR(
-    () => `/metrics?id=${data.projects[0].id}`,
-    _fetch
-  );
+  const { data } = useSWR('/projects', _fetch);
+  const { data: metricsData, error } = useSWR(() => `/metrics?id=${data.projects[0].id}`, _fetch);
 
   if (!data || !metricsData) {
     return <div>Loading...</div>;
   }
 
-  const chartData = metricsData.metrics.map((metric: any) => ({
-    name: metric.date,
-    ...metric.timings
-  }));
+  const chartData = takeRight(
+    metricsData.metrics.map((metric: any) => ({
+      name: metric.date,
+      ...metric.timings,
+    })),
+    72,
+  );
 
-  const bars = [
-    "total",
-    "dns",
-    "connect",
-    "tcp",
-    "firstByte",
-    "response",
-    "lookup"
-  ];
+  const bars = ['total', 'dns', 'connect', 'tcp', 'firstByte'];
 
   return (
     <div className="App">
       <BarChart
-        width={500}
+        width={600}
         height={300}
-        data={chartData}
+        data={chartData as any[]}
         margin={{
           top: 20,
           right: 30,
           left: 20,
-          bottom: 5
+          bottom: 5,
         }}
       >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="date" />
+        <XAxis dataKey="date" tick={false} />
         <YAxis />
         <Tooltip />
         <Legend />
@@ -64,6 +47,7 @@ const App: React.FC = () => {
           <Bar
             dataKey={bar}
             stackId="a"
+            key={bar}
             fill={`#${colorConvert.hsl.hex([100 + index * 45, 70, 50])}`}
           />
         ))}
